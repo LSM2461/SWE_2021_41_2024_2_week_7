d77da12da88934d8975527ef6f38105113fd166a
LSM2461 <imsumin26@gmail.com>
2024-10-19 11:29:05 +0900

    Updated function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
-    index_dict = {}
     
-    for i, num in enumerate(nums):
-        remain = target - num
-        if remain in index_dict:
-            return [index_dict[remain], i]
-        index_dict[num] = i
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
